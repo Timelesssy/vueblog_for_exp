16d93e3acfd43ff5659285c7414346a1f49f13c8
Hxcsy <1017536043@qq.com>
2024-10-26 15:20:07 +0800

    修改sql文件

--- db/blog.sql
+++ db/blog.sql
@@ -1,64 +1,70 @@
 /*
-SQLyog Professional v12.09 (64 bit)
-MySQL - 5.7.29-0ubuntu0.18.04.1 : Database - blog
-*********************************************************************
-*/
+ Navicat Premium Data Transfer
 
-/*!40101 SET NAMES utf8 */;
+ Source Server         : test01
+ Source Server Type    : MySQL
+ Source Server Version : 80036
+ Source Host           : localhost:3306
+ Source Schema         : blog
 
-/*!40101 SET SQL_MODE=''*/;
+ Target Server Type    : MySQL
+ Target Server Version : 80036
+ File Encoding         : 65001
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`blog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+ Date: 26/10/2024 15:07:17
+*/
 
-USE `blog`;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-/*Table structure for table `m_blog` */
-
+-- ----------------------------
+-- Table structure for m_blog
+-- ----------------------------
 DROP TABLE IF EXISTS `m_blog`;
-
-CREATE TABLE `m_blog` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `content` longtext,
+CREATE TABLE `m_blog`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
   `gmt_create` datetime NOT NULL,
-  `gmt_modified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `status` tinyint(4) DEFAULT NULL,
-  `deleted` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
+  `gmt_modified` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `status` tinyint NULL DEFAULT NULL,
+  `deleted` int NULL DEFAULT 0,
+  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-/*Data for the table `m_blog` */
+-- ----------------------------
+-- Records of m_blog
+-- ----------------------------
+INSERT INTO `m_blog` VALUES (18, 1, '4444', '44444', '444444', '2020-07-20 22:35:21', NULL, 0, 0, NULL);
+INSERT INTO `m_blog` VALUES (19, 1, '11111', '111', '11111', '2020-07-20 22:36:10', NULL, 0, 0, NULL);
+INSERT INTO `m_blog` VALUES (20, 1, '33333', '3333', '33333', '2020-07-20 23:52:50', NULL, 0, 0, NULL);
+INSERT INTO `m_blog` VALUES (21, 1, '4444', '44444', '44444', '2020-07-20 23:52:58', NULL, 0, 0, NULL);
+INSERT INTO `m_blog` VALUES (22, 1, '5555', '55555', '555555', '2020-07-20 23:53:07', '2020-07-21 21:25:15', 0, 1, NULL);
 
-insert  into `m_blog`(`id`,`user_id`,`title`,`description`,`content`,`gmt_create`,`gmt_modified`,`status`,`deleted`) values (18,1,'4444','44444','444444','2020-07-20 22:35:21',NULL,0,0),(19,1,'11111','111','11111','2020-07-20 22:36:10',NULL,0,0),(20,1,'33333','3333','33333','2020-07-20 23:52:50',NULL,0,0),(21,1,'4444','44444','44444','2020-07-20 23:52:58',NULL,0,0),(22,1,'5555','55555','555555','2020-07-20 23:53:07','2020-07-21 21:25:15',0,1);
-
-/*Table structure for table `m_user` */
-
+-- ----------------------------
+-- Table structure for m_user
+-- ----------------------------
 DROP TABLE IF EXISTS `m_user`;
+CREATE TABLE `m_user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `email` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `status` int NOT NULL DEFAULT 0,
+  `created` datetime NULL DEFAULT NULL,
+  `last_login` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `UK_USERNAME`(`username` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `m_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) DEFAULT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
-  `email` varchar(64) DEFAULT NULL,
-  `password` varchar(64) DEFAULT NULL,
-  `status` int(5) NOT NULL,
-  `created` datetime DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `UK_USERNAME` (`username`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of m_user
+-- ----------------------------
+INSERT INTO `m_user` VALUES (1, 'admin', 'https://cdn.jsdelivr.net/gh/HimitZH/CDN/images/HCODE.png', 'admin@qq.com', '96e79218965eb72c92a549dd5a330112', 0, '2020-07-19 22:00:00', NULL);
+INSERT INTO `m_user` VALUES (2, 'admin123', NULL, '1012145225@qq.com', 'e10adc3949ba59abbe56e057f20f883e', 0, NULL, NULL);
 
-/*Data for the table `m_user` */
-
-insert  into `m_user`(`id`,`username`,`avatar`,`email`,`password`,`status`,`created`,`last_login`) values (1,'admin','https://cdn.jsdelivr.net/gh/HimitZH/CDN/images/HCODE.png','admin@qq.com','96e79218965eb72c92a549dd5a330112',0,'2020-07-19 22:00:00',NULL);
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+SET FOREIGN_KEY_CHECKS = 1;
